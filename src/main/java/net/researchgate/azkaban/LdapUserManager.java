c9ae447227f83a1545ddbeba7264cc88c8bb40ba
Jiří Kozlovský <mail@jkozlovsky.cz>
2017-04-07 13:58:14 +0200

    Fixed login failure on referrals returned from LDAP
    Also rafactored a bit

--- src/main/java/net/researchgate/azkaban/LdapUserManager.java
+++ src/main/java/net/researchgate/azkaban/LdapUserManager.java
@@ -3,20 +3,26 @@ package net.researchgate.azkaban;
 import azkaban.user.*;
 import azkaban.utils.Props;
 import org.apache.directory.api.ldap.model.cursor.CursorException;
-import org.apache.directory.api.ldap.model.cursor.EntryCursor;
+import org.apache.directory.api.ldap.model.cursor.SearchCursor;
 import org.apache.directory.api.ldap.model.entry.Attribute;
 import org.apache.directory.api.ldap.model.entry.Entry;
 import org.apache.directory.api.ldap.model.entry.Value;
 import org.apache.directory.api.ldap.model.exception.LdapException;
+import org.apache.directory.api.ldap.model.exception.LdapInvalidAttributeValueException;
+import org.apache.directory.api.ldap.model.exception.LdapInvalidDnException;
 import org.apache.directory.api.ldap.model.filter.FilterEncoder;
+import org.apache.directory.api.ldap.model.message.SearchRequest;
+import org.apache.directory.api.ldap.model.message.SearchRequestImpl;
 import org.apache.directory.api.ldap.model.message.SearchScope;
+import org.apache.directory.api.ldap.model.name.Dn;
+import org.apache.directory.api.ldap.model.name.Rdn;
 import org.apache.directory.ldap.client.api.LdapConnection;
 import org.apache.directory.ldap.client.api.LdapNetworkConnection;
-
 
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LdapUserManager implements UserManager {
@@ -39,28 +45,40 @@ public class LdapUserManager implements UserManager {
     public static final String LOCAL_SALT_ACCOUNT = "user.manager.salt.account";
     public static final String LOCAL_SALT_PASSWORD = "user.manager.salt.password";
 
-    private String ldapHost;
-    private int ldapPort;
-    private boolean useSsl;
-    private String ldapUserBase;
-    private String ldapUserIdProperty;
-    private String ldapUEmailProperty;
-    private String ldapBindAccount;
-    private String ldapBindPassword;
-    private List<String> ldapAllowedGroups;
-    private List<String> ldapAdminGroups;
-    private String ldapGroupSearchBase;
-    private boolean ldapEmbeddedGroups;
+    private final String ldapHost;
+    private final int ldapPort;
+    private final boolean useSsl;
+    private final String ldapUserBase;
+    private final Dn ldapUserBaseDn;
+    private final String ldapUserIdProperty;
+    private final String ldapUEmailProperty;
+    private final String ldapBindAccount;
+    private final String ldapBindPassword;
+    private final List<String> ldapAllowedGroups;
+    private final List<String> ldapAdminGroups;
+    private final String ldapGroupSearchBase;
+    private final boolean ldapEmbeddedGroups;
 
     // Support local salt account for admin privileges
-    private String localSaltAccount;
-    private String localSaltPassword;
+    private final String localSaltAccount;
+    private final String localSaltPassword;
 
     public LdapUserManager(Props props) {
         ldapHost = props.getString(LDAP_HOST);
         ldapPort = props.getInt(LDAP_PORT);
         useSsl = props.getBoolean(LDAP_USE_SSL);
         ldapUserBase = props.getString(LDAP_USER_BASE);
+
+        Dn userBaseDn = null;
+        try {
+            userBaseDn = parseDn(ldapUserBase);
+        } catch (LdapInvalidDnException e) {
+            logger.error("Failed parsing user base DN: " + ldapUserBase);
+            e.printStackTrace();
+        }
+
+        ldapUserBaseDn = userBaseDn;
+
         ldapUserIdProperty = props.getString(LDAP_USER_ID_PROPERTY);
         ldapUEmailProperty = props.getString(LDAP_EMAIL_PROPERTY);
         ldapBindAccount = props.getString(LDAP_BIND_ACCOUNT);
@@ -70,12 +88,13 @@ public class LdapUserManager implements UserManager {
         ldapGroupSearchBase = props.getString(LDAP_GROUP_SEARCH_BASE);
         ldapEmbeddedGroups = props.getBoolean(LDAP_EMBEDDED_GROUPS, false);
         // Support local salt account for admin privileges
-        localSaltAccount = props.getString(LOCAL_SALT_ACCOUNT);
+        localSaltAccount = props.getString(LOCAL_SALT_ACCOUNT).trim();
         localSaltPassword = props.getString(LOCAL_SALT_PASSWORD);
     }
 
     @Override
     public User getUser(String username, String password) throws UserManagerException {
+        logger.info("Logging in user " + username);
         if (username == null || username.trim().isEmpty()) {
             throw new UserManagerException("Username is empty.");
         } else if (password == null || password.trim().isEmpty()) {
@@ -91,46 +110,63 @@ public class LdapUserManager implements UserManager {
         }
 
         LdapConnection connection = null;
-        EntryCursor result = null;
+        SearchCursor cursor = null;
 
         try {
             connection = getLdapConnection();
 
-            result = connection.search(
-                    ldapUserBase,
-                    "(" + escapeLDAPSearchFilter(ldapUserIdProperty + "=" + username) + ")",
-                    SearchScope.SUBTREE
-            );
+            SearchRequest searchRequest = new SearchRequestImpl();
 
-            if (!result.next()) {
+            String ldapSearchFilter = "(" + escapeLDAPSearchFilter(ldapUserIdProperty + "=" + username) + ")";
+            searchRequest.setFilter(ldapSearchFilter);
+            searchRequest.setScope(SearchScope.SUBTREE);
+            searchRequest.setBase(ldapUserBaseDn);
+
+            // This is important so that we can determine how many users was found only by iterating with cursor.next()
+            searchRequest.ignoreReferrals();
+
+            cursor = connection.search(searchRequest);
+
+            if (!cursor.next()) {
                 throw new UserManagerException("No user " + username + " found");
             }
 
-            final Entry entry = result.get();
+            final Entry entry = cursor.getEntry();
 
-            // FIXME: Our LDAP returns user twice
-            if (result.next()) {
-                throw new UserManagerException("More than one user found");
+            String userId = getUserId(entry);
+
+            // Check that only one user was found
+            while (cursor.next()) {
+
+                // May be referral, intermediate, done ...
+                if (cursor.isEntry()) {
+                    Entry nextEntry = cursor.getEntry();
+
+                    if (!getUserId(nextEntry).equals(userId)) {
+                        throw new UserManagerException("More than one user found");
+                    }
+                }
             }
 
             if (!isMemberOfGroups(connection, entry, ldapAllowedGroups)) {
                 throw new UserManagerException("User is not member of allowed groups");
             }
 
+            // Validate credentials
             connection.bind(entry.getDn(), password);
 
-            Attribute idAttribute = entry.get(ldapUserIdProperty);
-            Attribute emailAttribute = null;
+            User user = new User(userId);
+
             if (ldapUEmailProperty.length() > 0) {
-                emailAttribute = entry.get(ldapUEmailProperty);
+                Attribute emailAttribute = entry.get(ldapUEmailProperty);
+
+                if (emailAttribute != null)
+                    user.setEmail(emailAttribute.getString());
             }
 
-            if (idAttribute == null) {
-                throw new UserManagerException("Invalid id property name " + ldapUserIdProperty);
-            }
-            User user = new User(idAttribute.getString());
-            if (emailAttribute != null) {
-                user.setEmail(emailAttribute.getString());
+            for (String group : getMemberOfGroups(user, entry)) {
+                logger.info("User " + username + " is in group: " + group);
+                user.addGroup(group);
             }
 
             if (isMemberOfGroups(connection, entry, ldapAdminGroups)) {
@@ -145,8 +181,8 @@ public class LdapUserManager implements UserManager {
         } catch (CursorException e) {
             throw new UserManagerException("Cursor error", e);
         } finally {
-            if (result != null)
-                result.close();
+            if (cursor != null)
+                cursor.close();
 
             if (connection != null) {
                 try {
@@ -156,6 +192,53 @@ public class LdapUserManager implements UserManager {
                 }
             }
         }
+    }
+
+    /**
+     * Returns userId from configured userIdProperty
+     *
+     * @param entry
+     * @return
+     * @throws UserManagerException
+     * @throws LdapInvalidAttributeValueException
+     */
+    private String getUserId(Entry entry) throws UserManagerException, LdapInvalidAttributeValueException {
+
+        Attribute idAttribute = entry.get(ldapUserIdProperty);
+
+        if (idAttribute == null) {
+            throw new UserManagerException("Invalid id property name " + ldapUserIdProperty);
+        }
+
+        return idAttribute.getString();
+    }
+
+    /**
+     * Processes LDAP's "memberof" fields & parses the CN value, which is then added to returned list
+     *
+     * @param user
+     * @param entry
+     * @return memberOfGroups
+     */
+    private List<String> getMemberOfGroups(User user, Entry entry) {
+
+        List<String> memberOfGroups = new ArrayList<>();
+
+        Attribute groups = entry.get("memberof");
+
+        for (Value groupValue : groups) {
+            String groupLdap = (String) groupValue.getValue();
+
+            // Get only the CN value (from memberOf='cn=JON User Group,ou=groups,dc=example,dc=com')
+            String groupCn = groupLdap.split(",")[0];
+
+            // strip the "cn="
+            String group = groupCn.substring(3).toLowerCase();
+
+            memberOfGroups.add(group);
+        }
+
+        return memberOfGroups;
     }
 
     /**
@@ -252,40 +335,58 @@ public class LdapUserManager implements UserManager {
         }
 
         LdapConnection connection = null;
-        EntryCursor result = null;
+        SearchCursor cursor = null;
 
         try {
             connection = getLdapConnection();
 
-            result = connection.search(
-                    ldapUserBase,
-                    "(" + escapeLDAPSearchFilter(ldapUserIdProperty + "=" + username) + ")",
-                    SearchScope.SUBTREE
-            );
+            SearchRequest searchRequest = new SearchRequestImpl();
 
-            if (!result.next()) {
+            String ldapSearchFilter = "(" + escapeLDAPSearchFilter(ldapUserIdProperty + "=" + username) + ")";
+            searchRequest.setFilter(ldapSearchFilter);
+            searchRequest.setScope(SearchScope.SUBTREE);
+            searchRequest.setBase(ldapUserBaseDn);
+
+            // This is important so that we can determine how many users was found only by iterating with cursor.next()
+            searchRequest.ignoreReferrals();
+
+            cursor = connection.search(searchRequest);
+
+            if (!cursor.next()) {
                 logger.info("User doesn't exist");
                 return false;
             }
 
 
-            final Entry entry = result.get();
+            final Entry entry = cursor.getEntry();
 
             if (!isMemberOfGroups(connection, entry, ldapAllowedGroups)) {
                 logger.info("User is not in allowed groups" + ldapAllowedGroups);
                 return false;
             }
-            // FIXME: Our LDAP returns user twice
-            // Check if more than one user found
-            return !result.next();
+
+            String userId = getUserId(entry);
+
+            // Check that only one user was found
+            while (cursor.next()) {
+
+                // May be referral, intermediate, done ...
+                if (cursor.isEntry()) {
+                        return false;
+                }
+            }
+
+            return true;
 
         } catch (LdapException e) {
             return false;
         } catch (CursorException e) {
             return false;
+        } catch (UserManagerException e) {
+            return false;
         } finally {
-            if (result != null)
-                result.close();
+            if (cursor != null)
+                cursor.close();
 
             if (connection != null) {
                 try {
@@ -295,6 +396,26 @@ public class LdapUserManager implements UserManager {
                 }
             }
         }
+    }
+
+    /**
+     * Creates Dn object from string
+     *
+     * @param dnStr
+     * @return
+     * @throws LdapInvalidDnException
+     */
+    private Dn parseDn(String dnStr) throws LdapInvalidDnException {
+
+        Dn dn = new Dn();
+        String[] dnStrSplitted = dnStr.split(",");
+        for (int i = dnStrSplitted.length; i > 0; ) {
+            String rdnVal = dnStrSplitted[--i];
+            Rdn rdn = new Rdn(rdnVal);
+            dn = dn.add(rdn);
+        }
+
+        return dn;
     }
 
     @Override
